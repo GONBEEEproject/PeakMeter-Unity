a47f2d03cb9d89fbeefd6a4a91b38dde011f616d
ouchiminh <39828763+ouchiminh@users.noreply.github.com>
2021-09-15 00:00:57 +0900

    Update AudioLevelMeterTest.cs

--- Assets/Scripts/AudioLevelMeterTest.cs
+++ Assets/Scripts/AudioLevelMeterTest.cs
@@ -8,14 +8,14 @@ public class AudioLevelMeterTest : MonoBehaviour
     void Start()
     {
         meters = new ouchi.ILevelMeter[] {
-            new ouchi.RMSMeter(AudioSettings.outputSampleRate, new ouchi.Time(300, ouchi.Time.units.milliseconds)),
-            new ouchi.PeakMeter(AudioSettings.outputSampleRate)
+            new ouchi.PeakMeter(AudioSettings.outputSampleRate),
+            new ouchi.RMSMeter(AudioSettings.outputSampleRate, new ouchi.Time(300, ouchi.Time.units.milliseconds))
         };
 
         //ale = new ouchi.AudioLevelMeter(new ouchi.PeakMeter(AudioSettings.outputSampleRate), channel);
         ale = new ouchi.AudioLevelMeter(meters[meter], channel);
         transform.localScale = new Vector3(1, 0.125f, 1);
-        if(meter == 1)
+        if(meter == 0)
         {
             gameObject.GetComponent<Renderer>().material.color = Color.red;
         }
@@ -24,8 +24,8 @@ public class AudioLevelMeterTest : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        db = ale.GetLevel_dB();
-        transform.localPosition = new Vector3(1, (-mindb + System.Math.Max(db, mindb)) / -mindb * 10, 1f);
+        db = ale.GetLevel_dBSPL();
+        transform.localScale = new Vector3(1.0f / (meter + 1), (-mindb + System.Math.Max(db, mindb)) / -mindb * 20, 1f);
         Debug.Log("Update AudioLevelMeter");
     }
     ouchi.AudioLevelMeter ale;
